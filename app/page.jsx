7ebf0f2843a55e39fc8be78d274c1b3695aa510d
ashari-dev <boykid923@gmail.com>
2024-10-15 17:14:03 +0700

    feat:service page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,6 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
+import Start from "@/components/Start";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -8,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Fullstact Web Developer</span>
             <h1 className="h1 mb-6">
               Hello i'm <br />{" "}
@@ -29,13 +31,21 @@ const Home = () => {
                 <FiDownload />
               </Button>
 
-              <div className="mb-8 xl:mb-0"><Social containerStayle="flex gap-6" iconStayle="w-9 h-9 border border-accent rounded-full flex items-center justify-center text-center text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/></div>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStayle="flex gap-6"
+                  iconStayle="w-9 h-9 border border-accent rounded-full flex items-center justify-center text-center text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
             </div>
           </div>
           {/* photo */}
-          <div>Photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Start />
     </section>
   );
 };
---
+++ components/Start.jsx
@@ -0,0 +1,56 @@
+"use client";
+import CountUp from "react-countup";
+
+const start = [
+  {
+    num: 10,
+    text: "Yeart of experience",
+  },
+  {
+    num: 5,
+    text: "Projects completed",
+  },
+  {
+    num: 8,
+    text: "Tecnologies mastered",
+  },
+  {
+    num: 500,
+    text: "Code commits",
+  },
+];
+
+const Start = () => {
+  return (
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vh] mx-auto xl:max-w-none">
+          {start.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+              >
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-extrabold"
+                />
+                <p
+                  className={`${
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Start;
